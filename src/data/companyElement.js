0de8fe6225c69f8e0d74906d0b13bc8ac5e67de4
Gabrielecorti01 <gcorti.info@gmail.com>
2021-10-06 22:34:50 +0200

    Inizio main

---
+++ src/data/companyElement.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        icon: "",
+        title: "Tradition",
+        text: "lorem ipsum dolor sit amet consectureutu adipliscing elit."
+
+    },
+    {
+        icon: "",
+        title: "Security",
+        text: "lorem ipsum dolor sit amet consectureutu adipliscing elit."
+
+    },
+    {
+        icon: "",
+        title: "Certificate",
+        text: "lorem ipsum dolor sit amet consectureutu adipliscing elit."
+
+    },
+    {
+        icon: "",
+        title: "Expertise",
+        text: "lorem ipsum dolor sit amet consectureutu adipliscing elit."
+
+    },
+]
